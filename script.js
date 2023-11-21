67c33e62542a3f50f0f3e631407c694564a02ce5
salman <70580281+salman9011@users.noreply.github.com>
2023-11-21 21:42:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+// script.js
+
+document.getElementById("enterBtn").addEventListener("click", function() {
+    // Get the p tag by its id
+    var statusElement = document.getElementById("status");
+
+    // Create a new h1 element
+    var newHeading = document.createElement("h1");
+
+    // Set the text content of the new h1 element
+    newHeading.textContent = "Entered Metaverse";
+
+    // Replace the existing p tag with the new h1 element
+    statusElement.parentNode.replaceChild(newHeading, statusElement);
+});
